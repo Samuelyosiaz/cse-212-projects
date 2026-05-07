ec086a6c18287034f365ee52d4e5040924b1d0dc
Samuelyosiaz <samilyosiaz@gmail.com>
2026-05-07 13:07:58 -0600

    first two solutions of the midweek individual activitie

--- week01/teach/Program.cs
+++ week01/teach/Program.cs
@@ -1,5 +1,5 @@
-﻿Console.WriteLine("\n======================\nDivisors\n======================");
-Divisors.Run();
+﻿// Console.WriteLine("\n======================\nDivisors\n======================");
+// Divisors.Run();
 
 Console.WriteLine("\n======================\nArray Selector\n======================");
 ArraySelector.Run();
@@ -14,5 +14,5 @@ ArraySelector.Run();
 // Console.WriteLine("\n======================\nDivisors Solution\n======================");
 // DivisorsSolution.Run();
 //
-// Console.WriteLine("\n======================\nArray Selector Solution\n======================");
-// ArraySelectorSolution.Run();
+Console.WriteLine("\n======================\nArray Selector Solution\n======================");
+ArraySelectorSolution.Run();
