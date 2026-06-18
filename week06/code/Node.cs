673b1e84dbebec130cbdf6c5b73a04b4f97d9d78
Samuelyosiaz <samilyosiaz@gmail.com>
2026-06-18 16:23:24 -0600

    week 6 assigment

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,6 +12,11 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            // Do not insert duplicates
+            return;
+        }
 
         if (value < Data)
         {
@@ -34,12 +39,37 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+        if (value < Data)
+        {
+            return Left?.Contains(value) ?? false;
+        }
+        else
+        {
+            return Right?.Contains(value) ?? false;
+        }
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = 0;
+        int rightHeight = 0;
+
+        if (Left is not null)
+        {
+            leftHeight = Left.GetHeight();
+        }
+
+        if (Right is not null)
+        {
+            rightHeight = Right.GetHeight();
+        }
+
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
